3d0203ac2cdad47235b903a4465d763f43db4aae
imranabdisalan7 <agmraan@gmail.com>
2024-08-22 16:10:04 +0300

    kickstarting a lucrative web developer career

---
+++ 01-step.php
@@ -0,0 +1,17 @@
+<?php
+    echo"i ";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <h1> <?php echo 'php!';?></h1>
+    <h1>new</h1>
+    <h1>Dice Roll no:<?php echo rand(1,6);?></h1>
+</body>
+</html>
